0029a7fb1ac17cc1028ea281babdde811157a32f
ArulValan <arulvalanc@gmail.com>
2023-02-08 00:18:25 +0530

    feat(expand-all): #5 Added expand and collapse all

--- extension/src/webviews/components/Navbar.tsx
+++ extension/src/webviews/components/Navbar.tsx
@@ -1,10 +1,14 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import * as ReactDOM from "react-dom";
 
 // imports for the icons
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { faExpandAlt } from "@fortawesome/free-solid-svg-icons";
+import { faCompressAlt } from "@fortawesome/free-solid-svg-icons";
+import { renderProvider } from "../pages/sidebar";
+import { ExpandedState } from "../service/RenderProvider";
 
 const Navbar = ({ rootFile }: any) => {
   // onChange function that will send a message to the extension when the user selects a file
@@ -15,19 +19,43 @@ const Navbar = ({ rootFile }: any) => {
     if (filePath) {
       vscodeApi.postMessage({
         type: "onFile",
-        value: filePath
+        value: filePath,
       });
     }
   };
 
+  const expandAll =() => 
+    renderProvider.setGridExpandedState(ExpandedState.EXPANDED);
+
+  const collapseAll =() => 
+    renderProvider.setGridExpandedState(ExpandedState.COLLAPSED);
+
   // Render section
   return (
     <div className="navbar">
-      <input type="file" name="file" id="file" className="inputfile" onChange={(e) => {fileMessage(e);}}/>
+      <input
+        type="file"
+        name="file"
+        id="file"
+        className="inputfile"
+        onChange={(e) => {
+          fileMessage(e);
+        }}
+      />
       <label htmlFor="file">
-        <FontAwesomeIcon icon={faDownload}/>
-        <strong id="strong_file">{rootFile ? ` ${rootFile}` : ' Choose a file...'}</strong>
+        <FontAwesomeIcon icon={faDownload} />
+        <strong id="strong_file">
+          {rootFile ? ` ${rootFile}` : " Choose a file..."}
+        </strong>
       </label>
+      <div className='expand-collapse-buttons'>
+        <span>
+          <FontAwesomeIcon icon={faExpandAlt} onClick={expandAll} />
+        </span>
+        <span>
+          <FontAwesomeIcon icon={faCompressAlt} onClick={collapseAll}/>
+        </span>
+      </div>
     </div>
   );
 };
